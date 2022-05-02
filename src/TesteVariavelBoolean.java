f85b0ffa4c98dfa2a36bb1341e6a4727365eec94
Murilo Rocha <software@murilorocha.com.br>
2022-05-02 19:49:17 -0300

    Teste do uso de variável com o tipo de dados boolean em Java.

---
+++ src/TesteVariavelBoolean.java
@@ -0,0 +1,9 @@
+public class TesteVariavelBoolean {
+
+    public static void main(String[] args) {
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+        System.out.printf("Os valores do tipo booleano podem ser: %b e o %b", verdadeiro, falso);
+    }
+}
